a94f7a87020ed7ee5bfa59dcb1a646e2335a4748
Moh Aamir <mohaamir0706@gmail.com>
2025-12-15 16:16:54 +0530

    add the drag and drop logic using javascript

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelectorAll(".node").forEach((template) => {
     el.style.top = "100px";
     el.style.left = "100px";
     canvas.appendChild(el);
-    // makeDragable(el);
+    makeDragable(el);
 
     nodes.push({
       id: Date.now(),
@@ -21,3 +21,23 @@ document.querySelectorAll(".node").forEach((template) => {
     console.log(nodes[0].id);
   });
 });
+
+function makeDragable(el){
+  console.log("start dragging");
+  let offsetX,offsetY;
+  el.addEventListener('mousedown',(e)=>{
+    // console.log(e.clientX);
+    offsetX = e.clientX;
+    offsetY = e.clientY;
+    console.log(offsetX,offsetY);
+    // el.addEventListener("mousemove",mouseMovehandler);
+    // el.addEventListener("mouseUp",mouseUp);/
+    document.onmousemove = ev=>{
+      el.style.left = `${ev.pageX-offsetX}px`;
+      el.style.top = `${ev.pageY-offsetY}px`;
+    }
+    document.onmouseup=()=>{
+      document.onmousemove = null;
+    }
+  })
+}
