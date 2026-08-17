12662bb5fe106227ae08b0bab9e557b1f14491d3
Moh Aamir <mohaamir0706@gmail.com>
2026-08-17 18:57:03 +0530

    Comment out debug log in makeDragable function
    Commented out console log for mousedown event.

--- js/dragging.js
+++ js/dragging.js
@@ -5,7 +5,7 @@ export function makeDragable(el) {
   // console.log("start dragging");
   let offsetX, offsetY;
   el.addEventListener("mousedown", (e) => {
-     console.log("mousedown target:", e.target, "closest button:", e.target.closest("button"));
+     // console.log("mousedown target:", e.target, "closest button:", e.target.closest("button"));
     if(e.target.closest("button")) return;
     const rect = el.getBoundingClientRect();
     offsetX = e.clientX - rect.left;
